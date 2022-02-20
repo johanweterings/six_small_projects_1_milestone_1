183486450012c07acc89c0f29a201433bb96ac15
Johan Weterings <johan.weterings@gmail.com>
2022-02-20 17:30:58 +0100

    Add project files.

---
+++ binary_node1/BinaryNode.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace binary_node1
+{
+    public class BinaryNode<T>
+    {
+        public T? Value { get; set; }
+        public BinaryNode<T>? LeftChild { get; set; }
+        public BinaryNode<T>? RightChild { get; set; }
+
+        public BinaryNode()
+        {
+            Value = default;
+            LeftChild = null;
+            RightChild = null;
+        }
+
+        public void AddLeft(BinaryNode<T> child) 
+        {
+            LeftChild = child;
+        }
+
+        public void AddRight(BinaryNode<T> child)
+        {
+            RightChild = child;
+        }
+
+        public override string ToString()
+        {
+            var leftString = LeftChild?.Value.ToString() ?? "null";
+            var rightString = RightChild?.Value.ToString() ?? "null";
+            return $"{Value}: {leftString} {rightString}";
+        }
+    }
+}
